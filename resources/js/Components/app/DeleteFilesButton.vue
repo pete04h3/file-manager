5e71adb3953643c4c50feb52dc05bf9975a77151
pete04h3 <peetmj@gmail.com>
2023-11-15 13:45:00 +0100

    changes to styles

--- resources/js/Components/app/DeleteFilesButton.vue
+++ resources/js/Components/app/DeleteFilesButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button @click="onDeleteClick"
-            class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+            class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-red-500 :hover:border-red-500 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-red-500 dark:hover:border-red-500 dark:focus:ring-blue-500 dark:focus:text-white">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
              class="w-4 h-4 mr-2">
             <path stroke-linecap="round" stroke-linejoin="round"
